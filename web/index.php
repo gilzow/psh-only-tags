28e55ea5c96b9728b5ff8ad5ba1ae8f8b9b63b47
Paul Gilzow <paul.gilzow@platform.sh>
2023-11-07 12:48:55 -0600

    dirname not basename

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 $version = "unknown";
-if (is_file(basename(__DIR__).DIRECTORY_SEPARATOR.'tag-tracker.txt')) {
-    $version = file_get_contents(basename(__DIR__).DIRECTORY_SEPARATOR.'tag-tracker.txt');
+if (is_file(dirname(__DIR__).DIRECTORY_SEPARATOR.'tag-tracker.txt')) {
+    $version = file_get_contents(dirname(__DIR__).DIRECTORY_SEPARATOR.'tag-tracker.txt');
 }
 
 printf("<p>Current tag version is: %s</p>", $version);
